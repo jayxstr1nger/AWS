9fce0196f230485ffa8d9126c335296e82f3107a
jayxstr1nger <jays.powerbi@gmail.com>
2026-08-08 21:35:19 +0200

    Delete login.ps1

--- login.ps1
+++
@@ -1,4 +0,0 @@
-export AWS_ACCESS_KEY_ID="your-access-key"
-export AWS_SECRET_ACCESS_KEY="your-secret-key"
-export AWS_SESSION_TOKEN="your-session-token"  # if using MFA/STS
-export AWS_DEFAULT_REGION="us-east-1"
